c791e57a1376e55bc53b89fc7f69e90480dfcc6d
Daniel Penelva <d4n.andrade@gmail.com>
2024-06-12 02:30:46 -0300

    criando projeto fullstack spring boot

---
+++ src/main/java/com/api/demo_fullstack_springboot/DemoFullstackSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.api.demo_fullstack_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoFullstackSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoFullstackSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/demo_fullstack_springboot/DemoFullstackSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.demo_fullstack_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoFullstackSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
