2fd0f2807f43ac53873180aa0946a572d8542d91
Daniel Penelva <d4n.andrade@gmail.com>
2024-06-12 03:44:14 -0300

    Criando o CORS

--- src/main/java/com/api/demo_fullstack_springboot/controller/ClienteController.java
+++ src/main/java/com/api/demo_fullstack_springboot/controller/ClienteController.java
@@ -1,6 +1,7 @@
 package com.api.demo_fullstack_springboot.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +14,7 @@ import com.api.demo_fullstack_springboot.model.Cliente;
 import com.api.demo_fullstack_springboot.repository.ClienteRepository;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class ClienteController {
 
     @Autowired
